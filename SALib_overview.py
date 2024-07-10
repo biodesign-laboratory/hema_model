0c3c288846e7e92b40449e2219a4683013ef8d08
baileyfelicia-br <146391514+baileyfelicia-br@users.noreply.github.com>
2024-07-10 00:08:45 -0400

    Update SALib_overview.py

--- SALib_overview.py
+++ SALib_overview.py
@@ -7,29 +7,6 @@ from project_library import linear_sim
 import time
 import pandas
 
-# code block directly below is following SALib tutorial
-'''problem = {
-    'num_vars': 3,
-    'names': ['x1', 'x2', 'x3'],
-    'bounds': [[-1*np.pi, np.pi],
-               [-1*np.pi, np.pi],
-               [-1*np.pi, np.pi]]
-}       # dictionary defining the inputs to perform SA on
-
-param_values = saltelli.sample(problem, 1024)   # generates 1024*(2*3+2) input samples
-
-Y = Ishigami.evaluate(param_values)     # evaluates the Ishigami function on generated input samples
-
-Si = sobol.analyze(problem, Y)'''
-# Si is a py dictionary with keys "S1", "S2", "ST", "S1_conf", "S2_conf", "ST_conf"
-# the "_conf" keys store the corresponding confidence intervals (95%)
-
-'''print("First-order:", Si['S1'])
-print("Second-order:", Si['S2'])
-print("Total-order:", Si['ST'])'''
-
-
-# -----------------------------------------------------------------------------------------------------------------
 # code block directly below is using linear_sim function to evaluate sensitivity on the IHD model 
 problem_IHD = {
     'num_vars': 24,
@@ -63,7 +40,7 @@ problem_IHD = {
 
 # init_values = [1000, 0, 4000, 0, 0.9, 0.1, 1800, 300, 1] # chosen mostly arbitrarily
 
-param_values_IHD = sobol_sample.sample(problem_IHD, 1024)
+param_values_IHD = sobol_sample.sample(problem_IHD, 2000)
 print("Shape of the generated sample: ", param_values_IHD.shape)
 print("First few samples:")
 print(param_values_IHD[:3])
@@ -79,47 +56,48 @@ print("! Computations starting now !")
 start_time = time.time()
 for i, X in enumerate(param_values_IHD):
 
-    IHD_out[i] = linear_sim(init_values[i], X[:23], 0.01, 200, [100], [5000])[:, -1]        # for each function in the output data, grab only the last time-series data point
+    IHD_out[i] = linear_sim(init_values[i], X[:23], 0.01, 100, [50], [5000])[:, -1]        # for each function in the output data, grab only the last time-series data point
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print("Outputs generated for all input samples. Elapsed time: ", elapsed_time)
 
 
-SI_H = sobol_analyze.analyze(problem_IHD, IHD_out[:,0]),    # sensitivity indices for H(t) (HSPCs)
-SI_N = sobol_analyze.analyze(problem_IHD, IHD_out[:,1]),    # sensitivity indices for N(t) (Pathogens)
-SI_S = sobol_analyze.analyze(problem_IHD, IHD_out[:,4]),    # sensitivity indices for S(t) (Stable cells)
-SI_Q = sobol_analyze.analyze(problem_IHD, IHD_out[:,5]),    # sensitivity indices for Q(t) (Active cells)
-SI_U = sobol_analyze.analyze(problem_IHD, IHD_out[:,6]),    # sensitivity indices for U(t) (Immuno-suppressive cells)
-SI_I = sobol_analyze.analyze(problem_IHD, IHD_out[:,8])     # sensitivity indices for I(t) (Net Inflammation)
+SI_H = sobol_analyze.analyze(problem_IHD, IHD_out[:,0].flatten())     # sensitivity indices for H(t) (HSPCs)
+SI_N = sobol_analyze.analyze(problem_IHD, IHD_out[:,1].flatten())     # sensitivity indices for N(t) (Pathogens)
+SI_S = sobol_analyze.analyze(problem_IHD, IHD_out[:,4].flatten())     # sensitivity indices for S(t) (Stable cells)
+SI_Q = sobol_analyze.analyze(problem_IHD, IHD_out[:,5].flatten())     # sensitivity indices for Q(t) (Active cells)
+SI_U = sobol_analyze.analyze(problem_IHD, IHD_out[:,6].flatten())     # sensitivity indices for U(t) (Immuno-suppressive cells)
+SI_I = sobol_analyze.analyze(problem_IHD, IHD_out[:,8].flatten())     # sensitivity indices for I(t) (Net Inflammation)
+
 
 total_Si_H, first_Si_H, second_Si_H = SI_H.to_df()
-total_Si_N, first_Si_N, second_Si_N = SI_H.to_df()
-total_Si_S, first_Si_S, second_Si_S = SI_H.to_df()
-total_Si_Q, first_Si_Q, second_Si_Q = SI_H.to_df()
-total_Si_U, first_Si_U, second_Si_U = SI_H.to_df()
-total_Si_I, first_Si_I, second_Si_I = SI_H.to_df()
+total_Si_N, first_Si_N, second_Si_N = SI_N.to_df()
+total_Si_S, first_Si_S, second_Si_S = SI_S.to_df()
+total_Si_Q, first_Si_Q, second_Si_Q = SI_Q.to_df()
+total_Si_U, first_Si_U, second_Si_U = SI_U.to_df()
+total_Si_I, first_Si_I, second_Si_I = SI_I.to_df()
 
-total_Si_H.to_csv('total_Si_H.to_csv', sep='\t')
-first_Si_H.to_csv('first_Si_H.to_csv', sep='\t')
-second_Si_H.to_csv('second_Si_H.to_csv', sep='\t')
+total_Si_H.to_csv('total_Si_H.csv', sep='\t')
+first_Si_H.to_csv('first_Si_H.csv', sep='\t')
+second_Si_H.to_csv('second_Si_H.csv', sep='\t')
 
-total_Si_N.to_csv('total_Si_N.to_csv', sep='\t')
-first_Si_N.to_csv('first_Si_N.to_csv', sep='\t')
-second_Si_N.to_csv('second_Si_N.to_csv', sep='\t')
+total_Si_N.to_csv('total_Si_N.csv', sep='\t')
+first_Si_N.to_csv('first_Si_N.csv', sep='\t')
+second_Si_N.to_csv('second_Si_N.csv', sep='\t')
 
-total_Si_S.to_csv('total_Si_S.to_csv', sep='\t')
-first_Si_S.to_csv('first_Si_S.to_csv', sep='\t')
-second_Si_S.to_csv('second_Si_S.to_csv', sep='\t')
+total_Si_S.to_csv('total_Si_S.csv', sep='\t')
+first_Si_S.to_csv('first_Si_S.csv', sep='\t')
+second_Si_S.to_csv('second_Si_S.csv', sep='\t')
 
-total_Si_Q.to_csv('total_Si_Q.to_csv', sep='\t')
-first_Si_Q.to_csv('first_Si_Q.to_csv', sep='\t')
-second_Si_Q.to_csv('second_Si_Q.to_csv', sep='\t')
+total_Si_Q.to_csv('total_Si_Q.csv', sep='\t')
+first_Si_Q.to_csv('first_Si_Q.csv', sep='\t')
+second_Si_Q.to_csv('second_Si_Q.csv', sep='\t')
 
-total_Si_U.to_csv('total_Si_U.to_csv', sep='\t')
-first_Si_U.to_csv('first_Si_U.to_csv', sep='\t')
-second_Si_U.to_csv('second_Si_U.to_csv', sep='\t')
+total_Si_U.to_csv('total_Si_U.csv', sep='\t')
+first_Si_U.to_csv('first_Si_U.csv', sep='\t')
+second_Si_U.to_csv('second_Si_U.csv', sep='\t')
 
-total_Si_I.to_csv('total_Si_I.to_csv', sep='\t')
-first_Si_I.to_csv('first_Si_I.to_csv', sep='\t')
-second_Si_I.to_csv('second_Si_I.to_csv', sep='\t')
+total_Si_I.to_csv('total_Si_I.csv', sep='\t')
+first_Si_I.to_csv('first_Si_I.csv', sep='\t')
+second_Si_I.to_csv('second_Si_I.csv', sep='\t')
