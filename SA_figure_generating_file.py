16af7f1b083fd203fc10d08f041681f158b7fa6b
baileyfelicia-br <146391514+baileyfelicia-br@users.noreply.github.com>
2024-07-13 21:50:16 -0400

    Update SA_figure_generating_file.py

--- SA_figure_generating_file.py
+++ SA_figure_generating_file.py
@@ -7,30 +7,30 @@ import pandas as pd
 # ========== 1: Load data from csv's into useable format ==========
 time_array = np.arange(51, 75)
 
-t_1 = pd.read_csv('total_Si_H_2_51.csv', delimiter='\t')
-t_2 = pd.read_csv('total_Si_H_2_52.csv', delimiter='\t')
-t_3 = pd.read_csv('total_Si_H_2_53.csv', delimiter='\t')
-t_4 = pd.read_csv('total_Si_H_2_54.csv', delimiter='\t')
-t_5 = pd.read_csv('total_Si_H_2_55.csv', delimiter='\t')
-t_6 = pd.read_csv('total_Si_H_2_56.csv', delimiter='\t')
-t_7 = pd.read_csv('total_Si_H_2_57.csv', delimiter='\t')
-t_8 = pd.read_csv('total_Si_H_2_58.csv', delimiter='\t')
-t_9 = pd.read_csv('total_Si_H_2_59.csv', delimiter='\t')
-t_10 = pd.read_csv('total_Si_H_2_60.csv', delimiter='\t')
-t_11 = pd.read_csv('total_Si_H_2_61.csv', delimiter='\t')
-t_12 = pd.read_csv('total_Si_H_2_62.csv', delimiter='\t')
-t_13 = pd.read_csv('total_Si_H_2_63.csv', delimiter='\t')
-t_14 = pd.read_csv('total_Si_H_2_64.csv', delimiter='\t')
-t_15 = pd.read_csv('total_Si_H_2_65.csv', delimiter='\t')
-t_16 = pd.read_csv('total_Si_H_2_66.csv', delimiter='\t')
-t_17 = pd.read_csv('total_Si_H_2_67.csv', delimiter='\t')
-t_18 = pd.read_csv('total_Si_H_2_68.csv', delimiter='\t')
-t_19 = pd.read_csv('total_Si_H_2_69.csv', delimiter='\t')
-t_20 = pd.read_csv('total_Si_H_2_70.csv', delimiter='\t')
-t_21 = pd.read_csv('total_Si_H_2_71.csv', delimiter='\t')
-t_22 = pd.read_csv('total_Si_H_2_72.csv', delimiter='\t')
-t_23 = pd.read_csv('total_Si_H_2_73.csv', delimiter='\t')
-t_24 = pd.read_csv('total_Si_H_2_74.csv', delimiter='\t')
+t_1 = pd.read_csv('total_Si_N_2_51.csv', delimiter='\t')
+t_2 = pd.read_csv('total_Si_N_2_52.csv', delimiter='\t')
+t_3 = pd.read_csv('total_Si_N_2_53.csv', delimiter='\t')
+t_4 = pd.read_csv('total_Si_N_2_54.csv', delimiter='\t')
+t_5 = pd.read_csv('total_Si_N_2_55.csv', delimiter='\t')
+t_6 = pd.read_csv('total_Si_N_2_56.csv', delimiter='\t')
+t_7 = pd.read_csv('total_Si_N_2_57.csv', delimiter='\t')
+t_8 = pd.read_csv('total_Si_N_2_58.csv', delimiter='\t')
+t_9 = pd.read_csv('total_Si_N_2_59.csv', delimiter='\t')
+t_10 = pd.read_csv('total_Si_N_2_60.csv', delimiter='\t')
+t_11 = pd.read_csv('total_Si_N_2_61.csv', delimiter='\t')
+t_12 = pd.read_csv('total_Si_N_2_62.csv', delimiter='\t')
+t_13 = pd.read_csv('total_Si_N_2_63.csv', delimiter='\t')
+t_14 = pd.read_csv('total_Si_N_2_64.csv', delimiter='\t')
+t_15 = pd.read_csv('total_Si_N_2_65.csv', delimiter='\t')
+t_16 = pd.read_csv('total_Si_N_2_66.csv', delimiter='\t')
+t_17 = pd.read_csv('total_Si_N_2_67.csv', delimiter='\t')
+t_18 = pd.read_csv('total_Si_N_2_68.csv', delimiter='\t')
+t_19 = pd.read_csv('total_Si_N_2_69.csv', delimiter='\t')
+t_20 = pd.read_csv('total_Si_N_2_70.csv', delimiter='\t')
+t_21 = pd.read_csv('total_Si_N_2_71.csv', delimiter='\t')
+t_22 = pd.read_csv('total_Si_N_2_72.csv', delimiter='\t')
+t_23 = pd.read_csv('total_Si_N_2_73.csv', delimiter='\t')
+t_24 = pd.read_csv('total_Si_N_2_74.csv', delimiter='\t')
 
 t_x_arr = [t_1, t_2, t_3, t_4, t_5, t_6, t_7, t_8, t_9, t_10,
            t_11, t_12, t_13, t_14, t_15, t_16, t_17, t_18, t_19,
@@ -144,150 +144,69 @@ for t in range(24):
 
 # =============== 4. Plot data =============================
 
-fig, axs = plt.subplots()
+moderate_cutoff = np.zeros(24)+0.1
+high_cutoff = np.zeros(24)+0.3
 
-# plot g_n
-axs.plot(time_array, gn_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('g_n_sensitivity_over_time.png')
-plt.clf()
+si_arrays = [gn_si_arr, knq_si_arr, kns_si_arr, ktn_si_arr, w_si_arr, p_crit_si_arr,
+             spq_si_arr, sph_si_arr, sps_si_arr, sas_si_arr, sah_si_arr, sau_si_arr,
+             Is_si_arr, As_si_arr, Ic_si_arr, Ac_si_arr, y_si_arr, ds_si_arr,
+             dp_si_arr, da_si_arr, dq_si_arr, du_si_arr, H_init_si_arr, N_init_si_arr]
 
-# plot k_nq
-axs.plot(time_array, knq_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('k_nq_sensitivity_over_time.png')
-plt.clf()
+conf_arrays = [gn_conf_arr, knq_conf_arr, kns_conf_arr, ktn_conf_arr, w_conf_arr, p_crit_conf_arr,
+             spq_conf_arr, sph_conf_arr, sps_conf_arr, sas_conf_arr, sah_conf_arr, sau_conf_arr,
+             Is_conf_arr, As_conf_arr, Ic_conf_arr, Ac_conf_arr, y_conf_arr, ds_conf_arr,
+             dp_conf_arr, da_conf_arr, dq_conf_arr, du_conf_arr, H_init_conf_arr, N_init_conf_arr]
 
-# plot k_ns
-axs.plot(time_array, kns_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('k_ns_sensitivity_over_time.png')
-plt.clf()
+titles = ['$g_{n}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$k_{nq}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$k_{ns}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$k_{tn}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$\omega$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$P_{crit}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$S_{pq}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$S_{ph}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$S_{ps}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$S_{as}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$S_{ah}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$S_{au}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$I_{S}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$A_{S}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$I_{c}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$A_{C}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$\gamma$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$d_{s}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$d_{p}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$d_{a}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$d_{q}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$d_{u}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$H_{Init}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)',
+          '$N_{Init}$ Sensitivity Index During First 24 Hours of an Infection ($N(t)$)']
 
-# plot k_tn
-axs.plot(time_array, ktn_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('k_tn_sensitivity_over_time.png')
-plt.clf()
+filenames = ['gn_SI_2', 'knq_SI_2', 'kns_SI_2', 'ktn_SI_2', 'w_SI_2', 'p_crit_SI_2',
+             'spq_SI_2', 'sph_SI_2', 'sps_SI_2', 'sas_SI_2', 'sah_SI_2', 'sau_SI_2',
+             'Is_SI_2', 'As_SI_2', 'Ic_SI_2', 'Ac_SI_2', 'y_SI_2', 'ds_SI_2',
+             'dp_SI_2', 'da_SI_2', 'dq_SI_2', 'du_SI_2', 'H_init_SI_2', 'N_init_SI_2']
 
-# plot w
-axs.plot(time_array, w_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('w_sensitivity_over_time.png')
-plt.clf()
+for i in range(24):
 
-# plot p_crit
-axs.plot(time_array, p_crit_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('p_crit_sensitivity_over_time.png')
-plt.clf()
+    fig, axs = plt.subplots()
 
-# plot s_pq
-axs.plot(time_array, spq_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('s_pq_sensitivity_over_time.png')
-plt.clf()
+    axs.plot(time_array, si_arrays[i], 'k')  # SI indices
+    axs.plot(time_array, moderate_cutoff, 'y--', label='Mod. Influential lower bound')    # Lower bound on range to be considered moderately influential
+    axs.plot(time_array, high_cutoff, 'g--', label='Highly Influential lower bound')        # Lower bound on range to be considered highly influential
 
-# plot sph
-axs.plot(time_array, sph_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('s_ph_sensitivity_over_time.png')
-plt.clf()
+    axs.fill_between(
+    time_array, np.zeros(24)+si_arrays[i]-conf_arrays[i], np.zeros(24)+si_arrays[i]+conf_arrays[i], color='k', alpha=.15, label='$95\%$ Confidence Interval')
 
-# plot sps
-axs.plot(time_array, sps_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('s_ps_sensitivity_over_time.png')
-plt.clf()
+    axs.set_xlabel('Time ($t$)')
+    axs.set_ylabel('Sensitivity Index')
+    axs.set_ylim(ymin=0, ymax=1)
+    axs.set_title(titles[i])
+    axs.legend()
 
-# plot sas
-axs.plot(time_array, sas_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('s_as_sensitivity_over_time.png')
-plt.clf()
+    fig.savefig(filenames[i])
 
-# plot s_ah
-axs.plot(time_array, sah_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('s_ah_sensitivity_over_time.png')
-plt.clf()
+    plt.close(fig)
 
-# plot s_au
-axs.plot(time_array, sau_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('s_au_sensitivity_over_time.png')
-plt.clf()
-
-# plot I_s
-axs.plot(time_array, Is_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('I_s_sensitivity_over_time.png')
-plt.clf()
-
-# plot A_s
-axs.plot(time_array, As_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('A_s_sensitivity_over_time.png')
-plt.clf()
-
-# plot I_c
-axs.plot(time_array, Ic_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('I_c_sensitivity_over_time.png')
-plt.clf()
-
-# plot A_c
-axs.plot(time_array, Ac_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('A_c_sensitivity_over_time.png')
-plt.clf()
-
-# plot y
-axs.plot(time_array, y_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('y_sensitivity_over_time.png')
-plt.clf()
-
-# plot d_s
-axs.plot(time_array, ds_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('d_s_sensitivity_over_time.png')
-plt.clf()
-
-# plot d_p
-axs.plot(time_array, dp_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('d_p_sensitivity_over_time.png')
-plt.clf()
-
-# plot d_a
-axs.plot(time_array, da_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('d_a_sensitivity_over_time.png')
-plt.clf()
-
-# plot d_q
-axs.plot(time_array, dq_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('d_q_sensitivity_over_time.png')
-plt.clf()
-
-# plot d_u
-axs.plot(time_array, du_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('d_u_sensitivity_over_time.png')
-plt.clf()
-
-# plot H_init
-axs.plot(time_array, H_init_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('H_init_sensitivity_over_time.png')
-plt.clf()
-
-# plot N_init
-axs.plot(time_array, N_init_si_arr)
-axs.set_ylim( ymin=0, ymax=1)
-plt.savefig('N_init_sensitivity_over_time.png')
-plt.clf()
 
 print("Figures compiled succesfully!")
