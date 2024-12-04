a62e3528124f6b101e0c0ebecef41f689f1c7b5d
Youngmin Park <ympark1988@gmail.com>
2024-12-04 17:12:34 -0500

    renamed variables

--- yp_smooth_noreach.jl
+++ yp_smooth_noreach.jl
@@ -4,18 +4,18 @@ using Plots, DifferentialEquations
 
 # parameters
 K_h = 3
-d_h = 0.05
+d_H = 0.05
 theta_N = 2000
 theta_K = 5000
 tauQ = 0.5
 tauU = 0.5
 
-d_scsf = 0.3
-d_s = 0.8
-d_q = 0.9
-d_u = 0.8
-d_p = 0.95
-d_a = 0.95
+d_SCSF = 0.3
+d_S = 0.8
+d_Q = 0.9
+d_U = 0.8
+d_P = 0.95
+d_A = 0.95
 
 g_n = 0.1
 N_inf = 20000000
@@ -66,35 +66,38 @@ x[10] = K
 
 
 function biomodel(dx, x, p, t)
-    Hq=x[1];
-    I = (x[8] * ((x[3]^kk/(x[3]^kk + theta_N^kk)) + 0.25) * ((0.5 * (x[10]/((theta_K) + x[10]))) + 1)) / ((x[9] * ((theta_N^kk/(x[3]^kk + theta_N^kk)) + 0.25) * ((0.75 * (x[10]/((theta_K) + x[10]))) + 1)) + (x[8] * ((x[3]^kk/(x[3]^kk + theta_N^kk)) + 0.25) * ((0.5 * (x[10]/((theta_K) + x[10]))) + 1)))
-    nu_M = 0.2 * ((x[2] * x[9])/(0.2 * x[2] + x[9])) * (1 - I)
-    nu_Q = 0.2 * ((x[1] * x[8])/(0.2 * x[1] + x[8])) * I
+    Hq=x[1];Hm=x[2];N=x[3];S=x[4];Q=x[5];U=x[6];SCSF=x[7];P=x[8];A=x[9];K=x[10]
+    
+    I = (P * ((N^kk/(N^kk + theta_N^kk)) + 0.25) * ((0.5 * (K/(theta_K + K))) + 1)) / ((A * ((theta_N^kk/(N^kk + theta_N^kk)) + 0.25) * ((0.75 * (K/((theta_K) + K))) + 1)) + (P * ((N^kk/(N^kk + theta_N^kk)) + 0.25) * ((0.5 * (K/((theta_K) + K))) + 1)))
+       #(P * ((N^kk)/(theta_N^kk + N^kk) + 0.25) * ((0.5 * (K)/(theta_K + K) + 1))) / ((A * (theta_N^kk/(theta_N^kk + N^kk) + 0.25) * (0.75  *  (K)/(theta_K + K) + 1)) + (P * ((N^kk)/(theta_N^kk + N^kk) + 0.25) * ((0.5  *  (K)/(theta_K + K) + 1))))
+
+    println(I)
+    
+    nu_Q = 0.2 * ((Hq * P)/(0.2 * Hq + P)) * I
+    nu_M = 0.2 * ((Hm * A)/(0.2 * Hm + A)) * (1 - I)
+    
     beta = I / ((I_crit + 10) + I)
-    D_q = d_q * x[5]
-    D_u = d_u * x[6]
-    D_h = d_h * x[2]
-    D_p = d_p * x[8]
-    D_a = d_a * x[9]
-    D_s = d_s * x[4]
-    DI = (x[2] * (1/5) * x[8])/(x[2] + (1/5) * x[8]) * I
+    
+    DI = (Hm*P)/(Hm + 5*P) * I
     DQB = ((1/3) * tauQ * I)/(I_crit + I)
     DUB = ((1/3) * tauU ) * ((1/I) / (A_crit + (1/I)))
 
-    dx[1] = (nu_M - nu_Q) + (2 * x[1] * (1 - (K_h * x[1])/linearlike(x[7], 0.1, 0.00001)))
-    dx[2] = nu_Q - (DI * beta) - D_h - nu_M
-    dx[3] = g_n * x[2] * (1 - x[2]/N_inf) - (k_nq * x[5] + k_ns * x[4]) * (x[2]/(N_half + x[3]))
-    dx[4] = (DI * (1 - beta) + 2 * DI * beta) - DQB * x[4] - DUB * x[4] - D_s - (k_sn * x[3] * (x[4]/(k_sn * x[3] + x[4])))
-    dx[5] = DQB * x[4] - D_q * (1 - 0.5 * I / (2 + I) + 0.5)
-    dx[6] = DUB * x[4] - D_u
-    dx[7] = s_scsf * (K_crit /(K_crit + x[10])) - d_scsf * x[7]
-    dx[8] = (S_PH * x[2] + S_PQ * x[5]) * (0.5 * I / (2+ I) + 0.5) + S_PS * x[4] - D_p
-    dx[9] = (S_AH * x[2] + S_AU * x[6]) * (0.5 * I / (2+ I) + 0.5) + S_AS * x[4] - D_a
-    dx[10] = S_KD * (k_sn * x[3] * (x[4]/(k_sn * x[3] + x[4]))) - (R_KU * x[6] * (x[10]/K_crit * x[10]))
+    dHq = (nu_M - nu_Q) + (2 * Hq * (1 - (K_h * Hq)/linearlike(SCSF, 0.1, 0.00001)))
+    dHm = nu_Q - DI*beta - d_H*Hm - nu_M
+    dN = g_n * Hm * (1 - Hm/N_inf) - (k_nq * Q + k_ns * S) * (Hm/(N_half + N))
+    dS = (DI * (1 - beta) + 2 * DI * beta) - DQB * S - DUB * S - d_S * S - (k_sn * N * (S/(k_sn * N + S)))
+    dQ = DQB * S - d_Q * Q * (1 - 0.5 * I / (2 + I) + 0.5)
+    dU = DUB * S - d_U * U
+    dSCSF = s_scsf * (K_crit /(K_crit + K)) - d_SCSF * SCSF
+    dP = (S_PH * Hm + S_PQ * Q) * (0.5 * I / (2+ I) + 0.5) + S_PS * S - d_P * P
+    dA = (S_AH * Hm + S_AU * U) * (0.5 * I / (2+ I) + 0.5) + S_AS * S - d_A * A
+    dK = S_KD * (k_sn * N * (S/(k_sn * N + S))) - (R_KU * U * (K/K_crit * K))
+
+    dx[1]=dHq;dx[2]=dHm;dx[3]=dN;dx[4]=dS;dx[5]=dQ;dx[6]=dU;dx[7]=dSCSF;dx[8]=dP;dx[9]=dA;dx[10]=dK
     return dx
 end
 
-dx_test = Any[1,2,3,4,5,6,7,8,9,10]
+dx_test = Any[0,0,0,0,0,0,0,0,0,0]
 x_test = Any[1,2,3,4,5,6,7,8,9,10]
 
 # compare output to Bryan's RHS
