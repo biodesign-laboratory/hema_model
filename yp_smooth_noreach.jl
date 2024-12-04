b2ea170f68c1ad97cbd74697c86495a82f07a8ba
Youngmin Park <ympark1988@gmail.com>
2024-12-04 17:32:48 -0500

    rearranged order of variables inside the function to make outputs easier to compare to python

--- yp_smooth_noreach.jl
+++ yp_smooth_noreach.jl
@@ -51,27 +51,11 @@ function linearlike(x,c,e)
     c + (x - c) * (x^2 / (x^2 + e))
 end
 
-#=
-x[1] = Hq
-x[2] = Hm
-x[3] = N
-x[4] = S
-x[5] = Q
-x[6] = U
-x[7] = SCSF
-x[8] = P
-x[9] = A
-x[10] = K
-=#
-
 
 function biomodel(dx, x, p, t)
-    Hq=x[1];Hm=x[2];N=x[3];S=x[4];Q=x[5];U=x[6];SCSF=x[7];P=x[8];A=x[9];K=x[10]
+    Hq=x[1];Hm=x[2];N=x[3];P=x[4];A=x[5];SCSF=x[6];K=x[7];Q=x[8];S=x[9];U=x[10]
     
     I = (P * ((N^kk/(N^kk + theta_N^kk)) + 0.25) * ((0.5 * (K/(theta_K + K))) + 1)) / ((A * ((theta_N^kk/(N^kk + theta_N^kk)) + 0.25) * ((0.75 * (K/((theta_K) + K))) + 1)) + (P * ((N^kk/(N^kk + theta_N^kk)) + 0.25) * ((0.5 * (K/((theta_K) + K))) + 1)))
-       #(P * ((N^kk)/(theta_N^kk + N^kk) + 0.25) * ((0.5 * (K)/(theta_K + K) + 1))) / ((A * (theta_N^kk/(theta_N^kk + N^kk) + 0.25) * (0.75  *  (K)/(theta_K + K) + 1)) + (P * ((N^kk)/(theta_N^kk + N^kk) + 0.25) * ((0.5  *  (K)/(theta_K + K) + 1))))
-
-    println(I)
     
     nu_Q = 0.2 * ((Hq * P)/(0.2 * Hq + P)) * I
     nu_M = 0.2 * ((Hm * A)/(0.2 * Hm + A)) * (1 - I)
@@ -85,15 +69,17 @@ function biomodel(dx, x, p, t)
     dHq = (nu_M - nu_Q) + (2 * Hq * (1 - (K_h * Hq)/linearlike(SCSF, 0.1, 0.00001)))
     dHm = nu_Q - DI*beta - d_H*Hm - nu_M
     dN = g_n * Hm * (1 - Hm/N_inf) - (k_nq * Q + k_ns * S) * (Hm/(N_half + N))
-    dS = (DI * (1 - beta) + 2 * DI * beta) - DQB * S - DUB * S - d_S * S - (k_sn * N * (S/(k_sn * N + S)))
-    dQ = DQB * S - d_Q * Q * (1 - 0.5 * I / (2 + I) + 0.5)
-    dU = DUB * S - d_U * U
-    dSCSF = s_scsf * (K_crit /(K_crit + K)) - d_SCSF * SCSF
+
     dP = (S_PH * Hm + S_PQ * Q) * (0.5 * I / (2+ I) + 0.5) + S_PS * S - d_P * P
     dA = (S_AH * Hm + S_AU * U) * (0.5 * I / (2+ I) + 0.5) + S_AS * S - d_A * A
-    dK = S_KD * (k_sn * N * (S/(k_sn * N + S))) - (R_KU * U * (K/K_crit * K))
+    dSCSF = s_scsf * (K_crit /(K_crit + K)) - d_SCSF * SCSF
 
-    dx[1]=dHq;dx[2]=dHm;dx[3]=dN;dx[4]=dS;dx[5]=dQ;dx[6]=dU;dx[7]=dSCSF;dx[8]=dP;dx[9]=dA;dx[10]=dK
+    dK = S_KD * (k_sn * N * (S/(k_sn * N + S))) - (R_KU * U * (K/K_crit * K))
+    dQ = DQB * S - d_Q * Q * (1 - 0.5 * I / (2 + I) + 0.5)
+    dS = (DI * (1 - beta) + 2 * DI * beta) - DQB * S - DUB * S - d_S * S - (k_sn * N * (S/(k_sn * N + S)))
+    dU = DUB * S - d_U * U
+
+    dx[1]=dHq;dx[2]=dHm;dx[3]=dN;dx[4]=dP;dx[5]=dA;dx[6]=dSCSF;dx[7]=dK;dx[8]=dQ;dx[9]=dS;dx[10]=dU
     return dx
 end
 
